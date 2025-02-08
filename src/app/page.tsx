ff00a1e297e8146be83094a00a2348bb2f62e09b
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-02-08 18:19:27 +0530

    Customized Input and Select shadcn UI components heights to enhance application UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-4 p-11">
       <Button>Primary</Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="destructive">Destructive</Button>
@@ -12,6 +22,20 @@ export default function Home() {
       <Button variant="teritary">Teritary</Button>
       <Button disabled>Disabled</Button>
       <Button size="xs">Extra Small</Button>
+      <Input />
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Themes</SelectLabel>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
   );
 }
