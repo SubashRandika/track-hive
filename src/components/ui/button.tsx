658912fe3d110f80852c3f63f26dfe20de054daf
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-02-09 02:52:57 +0530

    Fix typo in buttonVariants for tertiary style border property.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
           "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground",
         muted: "bg-neutral-100 text-neutral-600 hover:bg-neutral-200/80",
         teritary:
-          "bg-blue-100 text-blue-600 border=transparent hover:bg-blue-200 shadow-none",
+          "bg-blue-100 text-blue-600 border-transparent hover:bg-blue-200 shadow-none",
       },
       size: {
         default: "h-10 px-4 py-2",
