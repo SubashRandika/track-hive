97edd8844442caece525e2b0603fb4c33848a61f
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-02-12 02:42:12 +0530

    Update AuthLayout to include dynamic navigation for Sign Up/Login based on current path.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface AuthLayoutProps {
@@ -7,12 +11,19 @@ interface AuthLayoutProps {
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathName = usePathname();
+  const isSignIn = pathName === "/sign-in";
+
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex items-center justify-between">
           <Image src="/logo.svg" alt="logo" width={152} height={56} />
-          <Button variant="secondary">Sign Up</Button>
+          <Button asChild variant="secondary">
+            <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+              {isSignIn ? "Sign Up" : "Login"}
+            </Link>
+          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
           {children}
