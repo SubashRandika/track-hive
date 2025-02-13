8fdfff8899c4af4212d6079da505b61038c639b1
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-02-14 00:24:42 +0530

    Add authentication API routes and implement login functionality with client to server type validation using hono.

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,8 +1,13 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+import auth from "@/features/auth/server/route";
+
 const app = new Hono().basePath("/api");
 
-app.get("/", (c) => c.json({ message: "Hello World!" }));
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = app.route("/auth", auth);
 
 export const GET = handle(app);
+
+export type AppType = typeof routes;
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -14,27 +14,26 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { z } from "zod";
-import Link from "next/link";
-
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(1, "Password is required"),
-});
+import { useLogin } from "../api/use-login";
+import { loginSchema } from "../schemas";
 
 export const SignInCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { mutate } = useLogin();
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    mutate(values);
   };
 
   return (
